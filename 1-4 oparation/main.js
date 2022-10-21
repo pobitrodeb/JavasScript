61fe946447b59a5624442207c560cc087f71ce4f
Pobitrodeb <pobitrodn@gmail.com>
2022-10-22 05:37:20 +0600

    subtraction oparator

--- 1-4 oparation/main.js
+++ 1-4 oparation/main.js
@@ -1,6 +1,9 @@
 var x = 18; 
 var y = 10; 
+
 var additionResult = x + y;
 console.log('The Total Result is', additionResult); // addition operator
 
 
+var subtractionResult = x - y; 
+console.log('The Total Result is', subtractionResult); //  subtraction operator
