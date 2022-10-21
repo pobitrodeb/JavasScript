fb8b2608dc4a85ce21e004fe032cd2a339ed51ab
Pobitrodeb <pobitrodn@gmail.com>
2022-10-22 05:15:20 +0600

    variable name correction and full nameing

--- 1-2 variable declear/main.js
+++ 1-2 variable declear/main.js
@@ -5,8 +5,8 @@ var var3 = 34;
 var total = var1+var3 ;
 console.log(total);
 
-var name1 = "learn with sumit"; 
-console.log(name1);
+var nameOfInstitue = "learn with sumit"; 
+console.log(nameOfInstitue);
 
 var firstName = 'Pobitro'; 
 var lastName = 'Debnath'; 
