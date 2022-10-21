8b203b7f69635d3e8b719644bb3728953509309f
Pobitrodeb <pobitrodn@gmail.com>
2022-10-22 05:08:41 +0600

    straing variable and concrete two vaiable

--- 1-2 variable declear/main.js
+++ 1-2 variable declear/main.js
@@ -4,3 +4,11 @@ var var1 = 33;
 var var3 = 34; 
 var total = var1+var3 ;
 console.log(total);
+
+var name1 = "learn with sumit"; 
+console.log(name1);
+
+var firstName = 'Pobitro'; 
+var lastName = 'Debnath'; 
+
+console.log(firstName,'',lastName);
