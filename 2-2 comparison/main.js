aebe878a04b030de0e31afae233c54e81c6c76d0
Pobitrodeb <pobitrodn@gmail.com>
2022-10-22 09:33:49 +0600

    Conditional (Ternary) Operator

--- 2-2 comparison/main.js
+++ 2-2 comparison/main.js
@@ -1,5 +1,6 @@
 var sajibAge = 23; 
 var shawnAge = 22; 
+var momin = 25; 
 
 // == Operator
 console.log(sajibAge == shawnAge); 
@@ -24,3 +25,9 @@ console.log(sajibAge <= shawnAge);
 
 // =< less than or equal to 
 console.log (shawnAge >= sajibAge); 
+
+
+// Conditional (Ternary) Operator
+console.log (sajibAge > shawnAge && sajibAge < momin); // if two condition is true return tru either fale result. 
+console.log(sajibAge > shawnAge || sajibAge < momin) ; // if one condition is true return true 
+console.log(!(sajibAge>shawnAge)) ; // 
