34bcf149bead5f060fc18d924729b28422b26ffd
Pobitrodeb <pobitrodn@gmail.com>
2022-10-22 07:20:03 +0600

    array index replace

--- 2-1 Array/main.js
+++ 2-1 Array/main.js
@@ -1,8 +1,12 @@
-var studentName = ['shafikul', 'rasel', 'shakhawat', 15, 18.15, true];  //delcear of array
+var studentName = ['shafikul', 'rasel', 'shakhawat', 'ariful', 'tilok', 'sajib', 'bipul'];  //delcear of array
 console.log(studentName);  
 
-var findIndex = studentName.indexOf(18.15); // find with arry value 
+var findIndex = studentName.indexOf('sajib'); // find with arry value 
 console.log(findIndex);
 
 var findIndexValue = studentName[2];  // find with index number 
 console.log(findIndexValue); 
+
+console.log('Now All Studentent are : ' + studentName); 
+studentName[5] = 'Guljar'; // replace value by indx numberr 
+console.log('Afer Replace All student : ' + studentName); 
