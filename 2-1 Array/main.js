c6b0b4e5f4babbcaa55863eb4090ab2b82ad6f1a
Pobitrodeb <pobitrodn@gmail.com>
2022-10-22 07:08:28 +0600

    find indexing number

--- 2-1 Array/main.js
+++ 2-1 Array/main.js
@@ -1,2 +1,5 @@
 var studentName = ['shafikul', 'rasel', 'shakhawat', 15, 18.15, true];  //delcear of array
 console.log(studentName);  
+
+var findIndex = studentName.indexOf(18.15);
+console.log(findIndex);
