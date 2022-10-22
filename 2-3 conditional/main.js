42902ec8e69cad969d949ba9c75de59673d5e933
Pobitrodeb <pobitrodn@gmail.com>
2022-10-22 09:58:07 +0600

    if else if condition example

---
+++ 2-3 conditional/main.js
@@ -0,0 +1,13 @@
+ var productQty = 150; 
+ 
+ if(productQty > 150)
+ {
+    console.log(productQty, 'Greater than 150'); 
+ }
+ else if(productQty => 150)
+ {
+    console.log(productQty, 'Greater than equal 150'); 
+ }
+ else{
+    console.log(productQty, 'Less than 150');
+ }
