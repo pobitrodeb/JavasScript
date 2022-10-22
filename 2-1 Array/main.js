d14506c00bbfee39773ab61c8d623ab06494579b
Pobitrodeb <pobitrodn@gmail.com>
2022-10-22 06:51:27 +0600

    Array folder created

---
+++ 2-1 Array/main.js
@@ -0,0 +1,11 @@
+var cheeckTypeOf = '200'; 
+console.log(typeof cheeckTypeOf); 
+
+var cheeckTypeOfSecound = 200; 
+console.log(typeof cheeckTypeOfSecound); 
+
+var cheeckTypeOfthird = true; 
+console.log(typeof cheeckTypeOfthird); 
+
+var today = new Date(); 
+console.log(typeof today); 
