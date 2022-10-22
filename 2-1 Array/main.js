e0da9d8c3564a009aaa2d9540754444bc02c133c
Pobitrodeb <pobitrodn@gmail.com>
2022-10-22 07:13:36 +0600

    find indexing number with arry index number

--- 2-1 Array/main.js
+++ 2-1 Array/main.js
@@ -1,5 +1,8 @@
 var studentName = ['shafikul', 'rasel', 'shakhawat', 15, 18.15, true];  //delcear of array
 console.log(studentName);  
 
-var findIndex = studentName.indexOf(18.15);
+var findIndex = studentName.indexOf(18.15); // find with arry value 
 console.log(findIndex);
+
+var findIndexValue = studentName[2];  // find with index number 
+console.log(findIndexValue); 
