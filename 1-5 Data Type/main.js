a5620c99f79bdadffe73865e3e522e50f73d2f36
Pobitrodeb <pobitrodn@gmail.com>
2022-10-22 06:33:59 +0600

    check data type

---
+++ 1-5 Data Type/main.js
@@ -0,0 +1,8 @@
+var cheeckTypeOf = '200'; 
+console.log(typeof cheeckTypeOf); 
+
+var cheeckTypeOfSecound = 200; 
+console.log(typeof cheeckTypeOfSecound); 
+
+var cheeckTypeOfthird = true; 
+console.log(typeof cheeckTypeOfthird); 
