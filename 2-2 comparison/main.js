1c89ca19634d9e0f4e18df04695a867168e0497d
Pobitrodeb <pobitrodn@gmail.com>
2022-10-22 09:05:36 +0600

    equal and not qual comparison operator

---
+++ 2-2 comparison/main.js
@@ -0,0 +1,15 @@
+var sajibAge = 23; 
+var shawnAge = 22; 
+
+// == Operator
+console.log(sajibAge == shawnAge); 
+
+// === Operator
+console.log(sajibAge === shawnAge); 
+
+// != operator 
+console.log (sajibAge != 100);
+
+// !== Operator 
+console.log(sajibAge !== shawnAge); 
+
