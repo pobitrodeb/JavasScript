93d319542900ff3080f919d38df3d43d471f389b
Pobitrodeb <pobitrodn@gmail.com>
2022-10-29 12:29:22 +0600

    complete while loop

--- 2-3 while loop/main.js
+++ 2-3 while loop/main.js
@@ -1,7 +1,17 @@
 var i = 0; 
+var valueOfCheckingStrat = 1; 
 
 while(i <= 10 )
 {
     console.log('The number is', i)
     i++;
+}
+
+//console.log('We are see How to check odd number with while loop')
+
+// odd numbers
+while(valueOfCheckingStrat <= 20)
+{
+    console. log(valueOfCheckingStrat); 
+    valueOfCheckingStrat = valueOfCheckingStrat + 2; 
 }
