c4a04f1690ac736c8bb6dd1f0d5d84e25cd55e33
Pobitrodeb <pobitrodn@gmail.com>
2022-10-29 13:08:56 +0600

    even and envent number checking using for loop

--- 2-4 for loop/main.js
+++ 2-4 for loop/main.js
@@ -3,3 +3,16 @@
 for (var i = 0; i < 5; i++) {
     console.log(i);
   }
+
+  // even and envent number checking using for loop 
+
+  for(var interValue = 0; interValue <= 20; interValue++)
+  {
+    if(interValue % 2 == 0)
+    {
+        console.log('This is even number : ',interValue); 
+    }
+    else{
+        console.log('This is odd numberr :',interValue); 
+    }
+  }
