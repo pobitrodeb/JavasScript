bedde5d4aa879e0f3744f9d5b1f4c5b866686ef3
Pobitrodeb <pobitrodn@gmail.com>
2022-10-30 09:41:43 +0600

    return funcation call

--- 3-2 funcation advanced/main.js
+++ 3-2 funcation advanced/main.js
@@ -6,3 +6,12 @@ function sleep(man, time)
 sleep("pobitro ", " 8pm");
 sleep("sajib  ", " 10pm");
 sleep("shawon  ", " 11pm");
+
+// Return Funcation 
+
+function numberMultiple(a, b) {
+    return a * b;             // Function returns the product of a and b
+  }
+
+  var numberIs = numberMultiple(15, 41);
+  console.log(numberIs);
