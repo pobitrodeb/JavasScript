46d617f0c24e94cade7deb0a720367b3ed43e197
Pobitrodeb <pobitrodn@gmail.com>
2022-11-09 04:23:03 +0600

    live show input value

--- 3-4 input_value_session/main.js
+++ 3-4 input_value_session/main.js
@@ -4,3 +4,9 @@ btn.onclick = function()
     var input = document.getElementById('inputValue');
     alert(input.value);
 }
+
+var input = document.getElementById('input');
+input.onkeyup = function()
+{
+    document.getElementById('inputValueText').innerHTML = input.value;
+}
