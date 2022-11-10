47cd3bd2847ae5a7ab090003f4e49bf676853567
Pobitrodeb <pobitrodn@gmail.com>
2022-11-11 02:18:15 +0600

    switch case complete

---
+++ 4-2 switch_case_ph/main.js
@@ -0,0 +1,27 @@
+
+switch('red')
+{
+    case 'blue' : 
+    console.log('This is color blue'); 
+    break; 
+
+    case 'red' : 
+    console.log('This is color red'); 
+    break; 
+
+    case 'white' : 
+    console.log('This is color white'); 
+    break; 
+
+    case 'yellow' : 
+    console.log('This is color yellow'); 
+    break; 
+
+    case 'dark' : 
+    console.log('This is color dark'); 
+    break; 
+
+    case 'nav-blue' : 
+    console.log('This is color nav-blue'); 
+    break; 
+}
