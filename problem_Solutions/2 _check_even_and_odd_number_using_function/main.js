c8fde6db9527f0062baa58db4ba84d0af46dcf27
Pobitrodeb <pobitrodn@gmail.com>
2022-11-12 05:27:53 +0600

    problem solution 2

---
+++ problem_Solutions/2 _check_even_and_odd_number_using_function/main.js
@@ -0,0 +1,12 @@
+function intoFeet(inchies)
+{
+    var feet = inchies / 12; 
+    return feet; 
+}
+
+var inputInchies  = 1230; 
+var feet          = intoFeet(inputInchies); 
+console.log(feet);
+
+
+
