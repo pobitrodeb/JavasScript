7bfdcb880bee45eca8fe74d79253ab996d3ad387
Pobitrodeb <pobitrodn@gmail.com>
2022-11-12 15:19:51 +0600

    problem solution 06

---
+++ problem_Solutions/5_ random number/main.js
@@ -0,0 +1,11 @@
+// JAVASCRIPT MATH 
+
+// Reference Site : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+var inputValue = -43;
+var absresult = Math.abs(inputValue);
+console.log(absresult);
+
+var totalAmount = 3343.54; 
+var roundResult = Math.round(totalAmount); 
+console.log(roundResult);
