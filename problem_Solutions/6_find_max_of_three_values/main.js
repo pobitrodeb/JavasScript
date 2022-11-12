f605c168c4539c1f0d2ab9b29119304090dfcfe8
Pobitrodeb <pobitrodn@gmail.com>
2022-11-12 15:41:35 +0600

    solution with math functing

--- problem_Solutions/6_find_max_of_three_values/main.js
+++ problem_Solutions/6_find_max_of_three_values/main.js
@@ -1,9 +1,5 @@
 // Basic Solution 
 
-// var firstNumber = 23; 
-// var secoundNumber = 34; 
-// var thirdNumber = 45; 
-
 function getChiking(firstNumber, secoundNumber, thirdNumber)
 {
     if(secoundNumber < firstNumber && thirdNumber < firstNumber)
@@ -22,3 +18,11 @@ function getChiking(firstNumber, secoundNumber, thirdNumber)
 
 var inputNumber = getChiking(45,54,84); 
 console.log(inputNumber); 
+
+
+// If you are want to  math 
+var firstNumber = 23; 
+var secoundNumber = 34; 
+var thirdNumber = 45; 
+var chekingBigNumber = Math.max(firstNumber, secoundNumber, thirdNumber); 
+console.log(chekingBigNumber); 
