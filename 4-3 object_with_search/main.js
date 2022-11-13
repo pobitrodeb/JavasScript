15de0505210533024d513d698d497d36bc521190
Pobitrodeb <pobitrodn@gmail.com>
2022-11-13 15:44:02 +0600

    object delcear and outpur

---
+++ 4-3 object_with_search/main.js
@@ -0,0 +1,16 @@
+var products = [
+    { product : 'chair',                    price: '2300' },
+    { product : 'table',                    price: '3300' },
+    { product : 'furniture',                price: '5300' },
+    { product : 'Locker',                   price: '2900' },
+    { product : 'Dressup table',            price: '2500' },
+    { product : 'Reading table',            price: '2800' }
+];
+
+function searchProducts(products, searchKey)
+{
+    for (var product of products)
+    console.log(products);
+}
+
+searchProducts(products);
