844dd5f41b1c3d0e7b99a0cc017bfe210f77d719
Pobitrodeb <pobitrodn@gmail.com>
2022-11-13 15:53:04 +0600

    searcing direct objec

--- 4-3 object_with_search/main.js
+++ 4-3 object_with_search/main.js
@@ -1,16 +1,16 @@
 var products = [
-    { product : 'chair',                    price: '2300' },
-    { product : 'table',                    price: '3300' },
-    { product : 'furniture',                price: '5300' },
-    { product : 'Locker',                   price: '2900' },
-    { product : 'Dressup table',            price: '2500' },
-    { product : 'Reading table',            price: '2800' }
+    { productName : 'chair',                    price: '2300' },
+    { productName : 'table',                    price: '3300' },
+    { productName : 'furniture',                price: '5300' },
+    { productName : 'Locker',                   price: '2900' },
+    { productName : 'Dressup table',            price: '2500' },
+    { productName : 'Reading table',            price: '2800' }
 ];
 
 function searchProducts(products, searchKey)
 {
     for (var product of products)
-    console.log(products);
+    console.log(product.price);
 }
 
 searchProducts(products);
