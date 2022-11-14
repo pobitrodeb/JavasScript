6bbdcf3bed2a3c67375247a509c1a1f1e15ab699
Pobitrodeb <pobitrodn@gmail.com>
2022-11-14 17:59:49 +0600

    complete Document Object Model

--- 5_DOM/js/main.js
+++ 5_DOM/js/main.js
@@ -1,4 +1,15 @@
-console.log(document.body);
+
 
 var blogTitle = document.getElementsByTagName('h3');
 console.log(blogTitle);
+
+for ( var totlaTag of blogTitle)
+{
+    console.log(totlaTag.innerText);
+}
+
+var blogDetail = document.getElementsByName('p'); 
+for(var blogparagraph of blogDetail)
+{
+    console.log(blogparagraph.innerText);
+}
