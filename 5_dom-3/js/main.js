4641558cee913256c0b5c0e46e2c956661121e1d
Pobitrodeb <pobitrodn@gmail.com>
2022-11-14 19:51:51 +0600

    catch class by js

---
+++ 5_dom-3/js/main.js
@@ -0,0 +1,12 @@
+var catchClass =  document.getElementsByClassName('name'); 
+
+for (var oneClass of catchClass)
+{
+    oneClass.style.color = 'white'; 
+    oneClass.style.backgroundColor = 'black'; 
+    oneClass.style.padding = '15px'; 
+    oneClass.style.margin = '15px'; 
+    oneClass.style.width = '350px'; 
+    oneClass.style.height = '85px'; 
+    oneClass.style.textAlign = 'center'; 
+}
