22a4e281ad1990dbba2c9bb0a62248d87a218283
Pobitrodeb <pobitrodn@gmail.com>
2022-11-14 18:58:12 +0600

    comple session

--- 5_DOM-2/js/main.js
+++ 5_DOM-2/js/main.js
@@ -2,3 +2,8 @@ var show =  document.getElementById('fist');
 
 show.style.backgroundColor = 'yellow';
 show.style.textAlign = 'center';
+
+
+var show2 = document.getElementById('secound').innerText; 
+
+show2.style.color = 'red';
