2a6c90bc54768bd6accec6f78624da96fa4183e0
Pobitrodeb <pobitrodn@gmail.com>
2022-11-20 14:08:12 +0600

    email validation commant

--- 6-4 form validation/main.js
+++ 6-4 form validation/main.js
@@ -8,7 +8,21 @@ $('#username').keyup(function (){
     {
         $('#nameError').text('Must be 5 charecter and less 21 ');
     }
-})
+});
+
+$('#email').keyup(function (){
+    var userEmail = $('#email').val(); 
+    var regex  = /^\w+@[a-zA-Z_]+?\.[a-zA-2]{5,10}$/;
+    if (regex.test(userEmail))
+    { 
+        $('#emailError').text(''); 
+    } 
+    else 
+    {
+     $('#emailError').text('name must be 3 to 20 chearecter')
+    }
+ });
+
 
 
 
