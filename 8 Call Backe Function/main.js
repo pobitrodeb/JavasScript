70ba52335ded71dd34df9093b0cfb1f2f67081c3
Pobitrodeb <pobitrodn@gmail.com>
2023-04-06 15:13:45 +0600

    Call Back Function Session Done

--- 8 Call Backe Function/main.js
+++ 8 Call Backe Function/main.js
@@ -1,4 +1,37 @@
 
-setTimeout(function(){
-    console.log('hello world');
-}, 5000)  //5000 is loading time 
+// setTimeout(function(){
+//     console.log('hello world');
+// }, 5000)  //5000 is loading time 
+
+/// Real life Call Back Function Uses 
+
+let allPersons  = [
+    {"name": "pobitro deb", "depatment": "CSE"},
+    {"name": "pobitro deb", "depatment": "CSE"},
+    {"name": "pobitro deb", "depatment": "CSE"},
+    {"name": "pobitro deb", "depatment": "CSE"},
+];
+
+function createPerson(person, callback){
+   setTimeout(function() {
+     allPersons.push(person) // add new person 
+
+   }, 1000);
+}
+
+// show view file for this function 
+function getData(){
+    setTimeout(function(){
+        let outputData = ''; 
+        allPersons.forEach(function(iteam){
+            outputData += `<li> ${iteam.name}</li>`;
+            document.getElementById('showData').innerHTML = outputData; 
+        })
+
+    }, 4000);
+}
+
+/// add person 
+createPerson({"name": "keyamoni", "depatment": "love"},getData); 
+// call back funtion 
+getData()
