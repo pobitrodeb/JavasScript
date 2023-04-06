6113b9d3fb86a2c0028079ac6e5113a9541cfb01
Pobitrodeb <pobitrodn@gmail.com>
2023-04-07 01:33:31 +0600

    folder nameing

--- 04 Array Data Converter/main.js
+++ 04 Array Data Converter/main.js
@@ -1,23 +1,23 @@
 // Balnk Array 
 var blankArray = []; 
 blankArray.push('name'); 
-// console.log(blankArray);
+console.log(blankArray);
 
 //String To Array 
 var divission = "Dhaka"; 
 var convertArray = divission.split();   //split method is convert string 
-// console.log(convertArray);
+console.log(convertArray);
 
 
 //Arry to String 
 var districts = "Dhaka"; 
 var convertArray = districts.split("");   //split method is convert string 
-// console.log(convertArray);       
+console.log(convertArray);       
 
 var countrys = ["Bangladesh", "Indida", "USA", "Italy"]; 
 // var result = countrys.toString(); 
 var result = countrys.join(" "); // Use Real time project uses 
-// console.log(result);
+console.log(result);
 
 
 //Multi Array Concret 
