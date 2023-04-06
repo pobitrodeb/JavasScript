f9a41b69980737bcbfdae8a5649a7362d6fb0ba7
Pobitrodeb <pobitrodn@gmail.com>
2023-04-07 00:40:19 +0600

    array mid lavel sorting , string converter

--- 2-2 Array Mid Level/main.js
+++ 2-2 Array Mid Level/main.js
@@ -1,3 +1,37 @@
+// Balnk Array 
 var blankArray = []; 
 blankArray.push('name'); 
-console.log(blankArray);
+// console.log(blankArray);
+
+//String To Array 
+var divission = "Dhaka"; 
+var convertArray = divission.split();   //split method is convert string 
+// console.log(convertArray);
+
+
+//Arry to String 
+var districts = "Dhaka"; 
+var convertArray = districts.split("");   //split method is convert string 
+// console.log(convertArray);       
+
+var countrys = ["Bangladesh", "Indida", "USA", "Italy"]; 
+// var result = countrys.toString(); 
+var result = countrys.join(" "); // Use Real time project uses 
+// console.log(result);
+
+
+//Multi Array Concret 
+var persons = ['Bimol', 'Rajib', 'Swapon', "Ajob"]; 
+var persons2 = ['anup', 'rahuls']; 
+ 
+var concat = persons.concat(persons2); 
+// console.log(concat);
+
+
+//Array Shorting 
+concat.sort(); // ascding sort 
+console.log(concat);  
+concat.reverse()
+console.log(concat);
+
+
