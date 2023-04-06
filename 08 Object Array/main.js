3fde0b5c2013c3d81f5ce919deb56d9abb273bfd
Pobitrodeb <pobitrodn@gmail.com>
2023-04-07 01:54:01 +0600

    catch data object

---
+++ 08 Object Array/main.js
@@ -0,0 +1,6 @@
+// Delcear 
+var arr = [10, 23, ['a', 'b', 'c']]; 
+// console.log(typeof(arr)); 
+console.log ('this is fist data ' + arr[1]);    // fist data 
+console.log('this is secound data ' + arr[2]);    // secound data 
+console.log('this is secound data with object ' + arr[2][1]); // secound data with object 
