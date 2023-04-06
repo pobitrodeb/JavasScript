36f08c8dce0481cafefa519ade63b9db05f5e76a
Pobitrodeb <pobitrodn@gmail.com>
2023-04-06 11:45:52 +0600

    call back function decleared

--- 7-1 Date Object/js/main.js
+++ 7-1 Date Object/js/main.js
@@ -1,10 +1,2 @@
-document.getElementById('commentSubmit').addEventListener('click', function(){
-  var comment = document.getElementById('commnetBox'); 
-  //var userComment =  console.log(comment.value);
-
-  var newComment = document.createElement('p');
-  newComment.innerText = comment.value;
-
-    var commentContainer = document.getElementById('commnet-container'); 
-    commentContainer.appendChild(newComment);
-})
+const d = new Date();
+console.log(d);
---
+++ 8 Call Backe Function/main.js
@@ -0,0 +1,4 @@
+
+setTimeout(function(){
+    console.log('hello world');
+}, 5000)  //5000 is loading time 
