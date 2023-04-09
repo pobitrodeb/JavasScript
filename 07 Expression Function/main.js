cdbb56c7328d0c5fc8ba6164968889781b545814
Pobitrodeb <pobitrodn@gmail.com>
2023-04-09 20:30:29 +0600

    arrow function concpet clear

--- 07 Expression Function/main.js
+++ 07 Expression Function/main.js
@@ -12,5 +12,11 @@ let something1 = function(secoundParams)
 {
   console.log("Call Somethins Funcation " + secoundParams);
 }
-
 something1("Secound Param");
+
+//Arrow Function 
+
+let something2 = (thirdParam) => {
+  console.log("Call Somethins Funcation " + thirdParam);
+}
+something2("Thirsd Param")
