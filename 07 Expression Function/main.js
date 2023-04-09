21c3390cd758bbe078cbb98f1ee446ed311c1e02
Pobitrodeb <pobitrodn@gmail.com>
2023-04-09 20:17:26 +0600

    expression funtion added

---
+++ 07 Expression Function/main.js
@@ -0,0 +1,16 @@
+//Normal Function 
+
+function something(firstParam)
+{
+  console.log("Call Somethins Funcation " + firstParam);
+}
+something("index0"); 
+
+//Expression Function 
+
+let something1 = function(secoundParams)
+{
+  console.log("Call Somethins Funcation " + secoundParams);
+}
+
+something1("Secound Param");
