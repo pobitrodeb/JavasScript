d5e59fb6cb094909acc33aead4600a92c0a6378f
Pobitrodeb <pobitrodn@gmail.com>
2023-04-09 22:50:06 +0600

    real life use able simple added

--- 04 Array Iteration Using For Each/main.js
+++ 04 Array Iteration Using For Each/main.js
@@ -4,7 +4,15 @@ var users = ['liton das', 'Shakib Al Hasan', 'Hero Alam'];
 // we will call 3 param calls  : 
 // 1st is eleement,   2nd is param index number, 3rd param is all data 
 
-users.forEach(function(value, i, all){
-    console.log(value, i, all);
-})
+// users.forEach(function(value, i, all){
+//     console.log(value, i, all);
+// })
+
+
+// ============ how to use real life  =================== 
+ let allData = function (item, i, all) { 
+    console.log(item);
+  }
+
+users.forEach(allData);
 
