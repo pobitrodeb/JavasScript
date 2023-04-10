5c9dd0f1f02b9d9c4215fdfe5a390f9af8e50392
Pobitrodeb <pobitrodn@gmail.com>
2023-04-10 18:55:33 +0600

    try catch with finally function data send sucessfully

--- 11 Error Handling/main.js
+++ 11 Error Handling/main.js
@@ -1,4 +1,13 @@
 
-// try catch 
+//error check by try catch 
 
-errorTest()
+console.log("before error");
+try {
+  
+} catch (error) {
+   console.log(error.message); // customize message 
+   console.log(error.name);  // error type show
+}finally{
+    console.log("I am inside finallay"); 
+}
+console.log('Result After Try Catch');
