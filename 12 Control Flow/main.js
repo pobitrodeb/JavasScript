a11537b0c6f9d6adacf0aebe164151fa82341d3e
Pobitrodeb <pobitrodn@gmail.com>
2023-04-10 19:42:05 +0600

    control flow session donee

---
+++ 12 Control Flow/main.js
@@ -0,0 +1,12 @@
+// Control Flow Means 
+// if, if else, else if 
+
+var valueNumber = prompt(); 
+
+if( valueNumber >= 50)
+{
+    console.log('this value  is less then 50');
+}
+else{
+    console.log('this value is greater than 50');
+}
