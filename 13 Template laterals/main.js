1dd30434b8ed394fb335dc3838bb0ac83fdbda0f
Pobitrodeb <pobitrodn@gmail.com>
2023-04-10 20:05:42 +0600

    lateral templete session added

---
+++ 13 Template laterals/main.js
@@ -0,0 +1,11 @@
+
+// string syntax 
+let a = "Bangladesh" ; 
+var resultA = a + " is my country";
+console.log(resultA);
+
+// literals 
+
+let u = "USA"; 
+var resultU = `${u} is my county`; 
+console.log(resultU);
