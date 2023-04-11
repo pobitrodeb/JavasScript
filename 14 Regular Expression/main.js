496472dad176e36bccbe10f93ebe33c23f417179
Pobitrodeb <pobitrodn@gmail.com>
2023-04-11 22:17:42 +0600

    match regular expression

--- 14 Regular Expression/main.js
+++ 14 Regular Expression/main.js
@@ -1,4 +1,12 @@
 let regularExperssion; 
 
 regularExperssion = / it is regular expression variable / ;
+regularExperssion = /It is string/
 console.log(regularExperssion);
+
+
+let str;
+str = "It is string"; 
+
+let result = regularExperssion.exec(str)
+console.log(result);
