5de6b32e40dd9d4861bf77e294034b56d46c26c6
Pobitrodeb <pobitrodn@gmail.com>
2023-04-11 22:34:11 +0600

    reular expression test function concepet

--- 14 Regular Expression/main.js
+++ 14 Regular Expression/main.js
@@ -3,10 +3,16 @@ let regularExperssion;
 regularExperssion = / it is regular expression variable / ;
 regularExperssion = /it is string/i
 console.log(regularExperssion);
+console.log(regularExperssion.resource);
 
 
 let str;
 str = "It is string"; 
 
+//exec ()
 let result = regularExperssion.exec(str)
+console.log(result);
+
+//test()
+let result2 = regularExperssion.test(str)
 console.log(result);
